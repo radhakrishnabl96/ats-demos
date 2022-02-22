cf9c791d4927c52392ce0cf313e4fcf01c4ae87f
Radhakrishna Bangalore Lakshmiprasad <radhakrishnabl96@gmail.com>
2022-02-22 15:08:30 +0100

    Spinup cluster changes

--- rk_model/Case1_calib_test_3yrs/ölSLURM_ats_test_2.sh
+++ rk_model/Case1_calib_test_3yrs/ölSLURM_ats_test_2.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -l
-#SBATCH --job-name=test_ats_2
+#SBATCH --job-name=test_ats
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=32
@@ -7,16 +7,16 @@
 #SBATCH --ntasks-per-core=1
 #SBATCH --mem-per-cpu=2G
 #SBATCH --time=00:20:00
-#SBATCH --partition=amo
+#SBATCH --partition=enos
 #SBATCH --constraint=[skylake|haswell]
 #SBATCH --mail-user=radhakrishna@hydromech.uni-hannover.de
 #SBATCH --mail-type=BEGIN,END,FAIL
-#SBATCH --output test_ats-job_2%j.out
-#SBATCH --error test_ats-job_2%j.err
+#SBATCH --output test_ats-job_%j.out
+#SBATCH --error test_ats-job_%j.err
  
 # Change to my work dir
-mkdir $SLURM_SUBMIT_DIR/test_slurm_2.demo
-cd $SLURM_SUBMIT_DIR/test_slurm_2.demo
+mkdir $SLURM_SUBMIT_DIR/test_slurm.demo
+cd $SLURM_SUBMIT_DIR/test_slurm.demo
 
 # Load modules
  
